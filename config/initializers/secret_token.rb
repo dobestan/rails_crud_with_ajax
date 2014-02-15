0fc17b9951d50a3835eb7960ff87a680a2469818
dobestan <dobestan@gmail.com>
2014-02-15 14:03:57 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsWithAjax::Application.config.secret_key_base = '781d878efa9656633b8f08af1b8252d7cb614946b508e23e7b35a1b0de6f165d3f6cfe8bada62f6b22f4ae56fffbd1998fdbabcb0d0a257e502d6f9887e9d8b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsWithAjax::Application.config.session_store :cookie_store, key: '_rails_with_AJAX_session'
